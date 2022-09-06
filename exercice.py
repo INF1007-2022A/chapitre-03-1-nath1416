7d27e51bb4c6d11e1482f411b5d80b10f71069c3
Nathan Bougie <202046701@edu.clg.qc.ca>
2022-09-06 19:22:54 -0400

    Premier essai

--- exercice.py
+++ exercice.py
@@ -5,32 +5,35 @@
 import math
 
 def square_root(a: float) -> float:
-    return 0.0
+    
+    return math.sqrt(a)
 
 
 def square(a: float) -> float:
-    return 0.0
+    return math.pow(a,2)
 
 
 def average(a: float, b: float, c: float) -> float:
-    return 0.0
+    return (a + b + c) / 3
 
 
 def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float:
-    return 0.0
+    sum = angle_degs + angle_mins / 60 + angle_secs / 3600
+
+    return sum * math.pi / 180
 
 
 def to_degrees(angle_rads: float) -> tuple:
+
     return 0.0, 0.0, 0.0
 
 
 def to_celsius(temperature: float) -> float:
-    return 0.0
+    return temperature * 1.8 + 32 
 
 
 def to_farenheit(temperature: float) -> float:
-    return 0.0
-
+    return temperature / 1.8 - 32
 
 def main() -> None:
     print(f"La racine carré de 144 est : {square_root(144)}")
